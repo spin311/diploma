9fd57690b3b640cf3e124698b6a7bfed0597026a
Svit Spindler <svit.spindler@gmail.com>
2024-03-02 12:36:58 +0100

    website initial

---
+++ website/scripts/main.js
@@ -0,0 +1,34 @@
+// output functions are configurable.  This one just appends some text
+// to a pre element.
+function outf(text) {
+    var mypre = document.getElementById("output");
+    mypre.innerHTML = mypre.innerHTML + text;
+}
+function builtinRead(x) {
+    if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+        throw "File not found: '" + x + "'";
+    return Sk.builtinFiles["files"][x];
+}
+
+// Here's everything you need to run a python program in skulpt
+// grab the code from your textarea
+// get a reference to your pre element for output
+// configure the output function
+// call Sk.importMainWithBody()
+function runit() {
+    const prog = document.getElementById("yourcode").value;
+    const mypre = document.getElementById("output");
+    mypre.innerHTML = '';
+    Sk.pre = "output";
+    Sk.configure({output:outf, read:builtinRead});
+    (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'mycanvas';
+    const myPromise = Sk.misceval.asyncToPromise(function () {
+        return Sk.importMainWithBody("<stdin>", false, prog, true);
+    });
+    myPromise.then(function(mod) {
+            console.log('success');
+        },
+        function(err) {
+            console.log(err.toString());
+        });
+}
